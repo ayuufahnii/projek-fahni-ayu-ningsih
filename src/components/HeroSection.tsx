6c2ba779aee151913056c6dfc39662e023fbadc0
ayuufahnii <fahniiayuu@gmail.com>
2026-03-15 18:43:02 +0700

    update nama

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,9 +38,9 @@ export default function HeroSection() {
             transition={{ duration: 0.8, delay: 0.3 }}
             className="font-display text-4xl md:text-6xl lg:text-7xl font-bold mb-6"
           >
-            Aspiring Fullstack
+            Hello
             <br />
-            <span className="text-gradient">Developer</span>
+            <span className="text-gradient">I'am Fahni Ayu Ningsih</span>
           </motion.h1>
 
           <motion.p
