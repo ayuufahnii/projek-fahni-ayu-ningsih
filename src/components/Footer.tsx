60c9cac7ce3bb4936c233c335f53bf237f824d3e
ayuufahnii <fahniiayuu@gmail.com>
2026-03-16 20:51:53 +0700

    update bawah

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: Github, href: '#', label: 'GitHub' },
+    { icon: Github, href: 'https://github.com/ayuufahnii/projek-fahni-ayu-ningsih.git', label: 'GitHub' },
     { icon: Linkedin, href: '#', label: 'LinkedIn' },
     { icon: Youtube, href: '#', label: 'YouTube' },
     { icon: Instagram, href: '#', label: 'Instagram' },
